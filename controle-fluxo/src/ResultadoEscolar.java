b9493770af2a1f554bd092092338fde4649afeac
PLGVS <pedro.gaspar1214@gmail.com>
2023-09-01 17:13:44 -0300

    Curso de Estruturas de Controle

---
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,20 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        double nota = 8;
+        /* 
+        if(nota >= 7){
+            System.out.println("Aprovado!");
+        }
+        else if(nota >= 5 && nota < 7){
+            System.out.println("Recuperação!");
+        }
+        else{
+            System.out.println("Reprovado!");
+        }
+        */
+
+        String resultado = nota >= 7 ? "Aprovado!" : "Reprovado!";
+
+        System.out.println(resultado);
+    }
+}
